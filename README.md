20dede4f6392b8ada515916e8a9e8d15705ca06c
Noam Chitayat <noam.chitayat+git@gmail.com>
2015-03-04 23:16:45 -0800

    Initial commit
    PageMod extension to add a volume control to Bandcamp album pages,
    along with README and LICENSE.

---
+++ README.md
@@ -0,0 +1,22 @@
+Bandcamp Volume Control
+===========
+
+This Firefox extension adds a volume slider underneath the playback controls on Bandcamp's album pages.
+
+To package and use the extension directly from source:
+
+1. Install the [Firefox Add-on SDK](https://developer.mozilla.org/en-US/Add-ons/SDK/Tutorials/Installation).
+2. In your local copy of this repo, run `cfx xpi`.
+3. Using Firefox, open the .xpi file that you created in step 2.
+
+License and Copyright
+-----
+
+This extension and its source code are Copyright 2015 Noam Chitayat.
+
+The source code is made available under the terms of the Mozilla Public License (Version 2.0). For the full text of the license in use, please refer to the LICENSE.md file.
+
+Disclaimer
+-----
+
+This is an *unofficial* extension created by a fan for a very specific use case. Bandcamp does not support or endorse this extension in any way, shape or form. Bandcamp may, at any time, alter their user interface in a way that causes this extension to fail, which may lead to malfunction of Bandcamp's pages while the extension is active. Neither Bandcamp nor any contributor to this extension is responsible for any damages resulting from use of the extension. The extension and source code are provided AS IS, with no warranty of any kind.
---
+++ data/volumeControl.js
@@ -0,0 +1,67 @@
+(function() {
+   var playerCell = document.querySelector(".inline_player");
+   if(!playerCell) {
+      // By this time, the Bandcamp page has already loaded.
+      // However, if there is no audio player div to add a volume control to
+      // at this point, then there's no need to add a volume control.
+      return;
+   }
+
+   // If we have an audio element on the page,
+   // initialize the volume slider with its volume.
+   var audio = document.querySelector("audio");
+   var startVolume = 0.7;
+   
+   if(audio) {
+      startVolume = audio.volume;
+   }
+
+   // Create the volume slider itself
+   var volumeControl = document.createElement("input");
+   volumeControl.setAttribute("type", "range");
+   volumeControl.setAttribute("min", "0");
+   volumeControl.setAttribute("max", "100");
+   volumeControl.className = "volumeControl";
+
+   var volumeIcon = document.createElement("span");
+   volumeIcon.className = "ui-icon ui-icon-volume-on volumeIcon";
+
+   // Create a div to contain both the volume control
+   // and its icon 
+   var volumeDiv = document.createElement("div");
+   volumeDiv.appendChild(volumeIcon);
+   volumeDiv.appendChild(volumeControl);
+
+   var updateAudioElements = function () {
+      // Bootstrap has two volume icons (on/off)
+      // Toggle between the two depending on whether
+      // the volume slider is at zero or not.
+
+      var classToAdd = "ui-icon-volume-on";
+      var classToRemove = "ui-icon-volume-off";
+
+      if(volumeControl.value === "0") {
+         var classToAdd = "ui-icon-volume-off";
+         var classToRemove = "ui-icon-volume-on";
+      }
+
+      volumeIcon.classList.add(classToAdd);
+      volumeIcon.classList.remove(classToRemove);
+
+      // Update volume on every <audio> element on the page
+      var audioElements = document.querySelectorAll("audio");
+      if(audioElements) {
+         Array.prototype.forEach.call(audioElements, function () {
+            audio.volume = (volumeControl.value / 100);
+         });
+      }
+   };
+
+   // Listen for input *and* change so we update volume as
+   // the volume scrubber is being dragged.
+   volumeControl.addEventListener("input", updateAudioElements);
+   volumeControl.addEventListener("change", updateAudioElements);
+
+   // Place the volume slider near the inline player
+   playerCell.appendChild(volumeDiv);
+})();
